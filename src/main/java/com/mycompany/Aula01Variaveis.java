6f80bd2f1a40c596f795a04caf6d6aa45ae59aae
thamyrismilanez <thamyris.milanez@escola.pr.gov.br>
2025-03-26 13:51:32 +0000

    Aula 01 Variaveis

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,15 @@
+package com.my.company;
+
+public class Aula01Variaveis {
+
+    public static void main(String[] args) {
+        // Variavel lógica 
+
+        //Tipo primitivo
+
+        boolean ValorLogicoPrimitivo = true;
+
+        //Tipo abstrato
+        Boolean valorLogicoAbstrato = false;
+    }
+}
