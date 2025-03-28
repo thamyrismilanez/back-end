621d49198b18e035cefb940636a716e18ed640c7
thamyrismilanez <thamyris.milanez@escola.pr.gov.br>
2025-03-28 13:43:01 +0000

    Aula 2 sobre variaveis

--- src/main/java/com/mycompany/Aula01Variaveis.java
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -16,6 +16,31 @@ public class Aula01Variaveis {
         // Exibe o texto
         System.out.println("O valor da variavel valorLogicoAbstrato é: " + valorLogicoPrimitivo);
         
+        // Variáveis inteiras 
+
+        // Tipo primitivo
+        int valorInteiro = 10; //32bits
+        long valorInteiroLongo = 10L; // 64bits
+
+        // Tipo abstrato 
+        Integer valorInteiroAbs = 20;
+        Long valorLongo = 20L; 
+
+        System.out,println("O valor de valorInteiro é: " + valorInteiro + ", e o valor de 
+        valorInteiroLongo é : " + valorInteiro);
+
+        // Variáveis Decimais 
+
+        // Tipo Primitivo
+        float numeroFloat = 10.5f; // 32 =bits
+        double numeroDoibleAbs = 10.5; //64bits
+
+        //Tipo abstrato
+        Float numeroFloat = 10.5f; 
+        Double numeroDoibleAbs = 10.5;
+
+        Sytem.out.println("O valor de numeroFloat é: " + numeroFloat + ", e o valor de numeroDouble é: "
+        + numeroDouble);
 
     }
 }
