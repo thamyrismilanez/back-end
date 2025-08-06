36c8281d586f5cf380a745ddd94b4d56edceee72
thamyrismilanez <thamyris.milanez@escola.pr.gov.br>
2025-08-06 19:11:07 +0000

    aula 10, modificadores de acesso, parte 2

--- src/main/java/com/mycompany/poo/aula10/App.java
+++ src/main/java/com/mycompany/poo/aula10/App.java
@@ -1,34 +1,39 @@
 package com.mycompany.poo.aula10;
 
 public class App {
-
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
-        pessoa.nome = "thamyris";
-        pessoa.idade = 16
-        //pessoa.salario = 1.000; atrubutos privados não são visíveis 
+        pessoa.nome = "Dani";
+        pessoa.idade = 17;
+        // pessoa.salario = 1.000; atributos privados não são visiveis
 
-        // Instaciado classes que estão declaradas em outros arquivos
         Pessoa pessoa1 = new Pessoa();
         Pessoa pessoa2 = new Pessoa();
 
-        pessoa1.nome = "João";
-        pessoa1.idade = 28;
-        pessoa1.altura = 1.78;
+        pessoa1.nome = "Renan";
+        pessoa1.idade = 20;
+        pessoa1.altura = 1.60;
 
-        pessoa2.nome = "Maria";
-        pessoa2.idade = 38;
-        pessoa2.altura = 1.65;
+        pessoa2.nome = "Jean";
+        pessoa2.idade = 26;
+        pessoa2.altura = 1.70;
 
-        System.out.println("--- Dados da pessoa ---");
+        pessoa1.exibirDados();
+        pessoa2.exibirDados();
+
+        pessoa1.atribuirSalario(2000.0, 500);
+        pessoa2.atribuirSalario(3500.0);
+
+        /*System.out.println(" --- Dados da pessoa --- ");
         System.out.println("Nome: " + pessoa1.nome);
         System.out.println("Idade: " + pessoa1.idade);
-        System.out.println("Altura (m): " + pessoal.altura);
+        System.out.println("Altura (m):" + pessoa1.altura);
 
         System.out.println("--- Dados da pessoa ---");
-        System.out.println("Nome: " + pessoa2.nome);
+        System.out.println("Nome :  " + pessoa2.nome);
         System.out.println("Idade: " + pessoa2.idade);
-        System.out.println("Altura (m): " + pessoa2.altura);
+        System.out.println("Altura (m):" + pessoa2.altura);*/
 
     }
+
 }
